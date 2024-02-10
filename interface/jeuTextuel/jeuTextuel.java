4ec4371ea7e7afa0fc3e1a17b138338c39340337
Ahmed Amine Benhammada <123604915+KESHRUD@users.noreply.github.com>
2024-02-10 15:47:13 +0100

    Update jeuTextuel.java
    Quelques changements

--- interface/jeuTextuel/jeuTextuel.java
+++ interface/jeuTextuel/jeuTextuel.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import src.main.java.Instruction;
 
-public class jeuTextuel{
-    public jeuTextuel() {
+public class JeuTextuel{
+    public JeuTextuel() {
         boolean jeuEnCours = true;
 
         String menu;
