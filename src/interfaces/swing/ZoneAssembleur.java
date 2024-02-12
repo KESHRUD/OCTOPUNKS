cdb76366b9108005460ed22832bb648366c42c55
Stevekgs28 <84630495+Stevekgs28@users.noreply.github.com>
2024-02-12 22:19:48 +0100

    Ajout de méthodes pour (dé)bloquer la saisie de code assembleur
    Il y a aussi une deuxième zone de saisie d'assembleur

--- src/interfaces/swing/ZoneAssembleur.java
+++ src/interfaces/swing/ZoneAssembleur.java
@@ -1,29 +1,76 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import java.awt.Color;
 
 
 public class ZoneAssembleur extends JPanel
 {
+    private JLabel labelAssembleur;
+    private JScrollPane scrollBar1;
+    private JScrollPane scrollBar2;
+
+    private JTextArea zoneTexte1;
+    private JTextArea zoneTexte2;
+
+
     /**
-     * Zone de texte pour mettre les instructions assembleur.
-     * @param panel le panel qui contiendra la zone de texte.
-     */
+     * On crée la zone qui concerne les entrées de code assembleur.
+    */
     public ZoneAssembleur()
     {
         this.setBounds(770,100,400,600);
         this.setBackground(Color.RED);
         this.setLayout(null);
 
-        JLabel labelAssembleur = new JLabel("Instructions assembleur");
+        labelAssembleur = new JLabel("Instructions assembleur");
         labelAssembleur.setBounds(30,30,160,10);
         this.add(labelAssembleur);
 
-        JTextArea zoneTexte = new JTextArea();
-        zoneTexte.setBounds(10,60,375,450);
+        zoneTexte1 = new JTextArea();
+        zoneTexte1.setBounds(10,60,375,225);
+        scrollBar1 = new JScrollPane(zoneTexte1);
+        scrollBar1.setBounds(zoneTexte1.getX(), zoneTexte1.getY(), zoneTexte1.getWidth(), zoneTexte1.getHeight());
+        this.add(scrollBar1);
 
-        JScrollPane scrollBar = new JScrollPane(zoneTexte);
-        scrollBar.setBounds(10, 60, 375, 450);
+        zoneTexte2 = new JTextArea();
+        zoneTexte2.setBounds(zoneTexte1.getX(),zoneTexte1.getHeight()+zoneTexte1.getY()+20,375,225);
+        scrollBar2 = new JScrollPane(zoneTexte2);
+        scrollBar2.setBounds(zoneTexte2.getX(), zoneTexte2.getY(), zoneTexte2.getWidth(), zoneTexte2.getHeight());
+        this.add(scrollBar2);
+    }
 
-        this.add(scrollBar);
+    /**
+     * Permet de bloquer la modification d'une zone de saisie de code assembleur
+     * @param zoneTexte la zone de saisie du code assembleur
+     */
+    public void disableModification(JTextArea zoneTexte)
+    {
+        zoneTexte.setEditable(false);
+    }
+
+    /**
+     * Permet d'autoriser la modification d'une zone de saisie de code assembleur
+     * @param zoneTexte la zone de saisie du code assembleur
+     */
+    public void enableModification(JTextArea zoneTexte)
+    {
+        zoneTexte.setEditable(true);
+    }
+
+
+    /**
+     * Renvoie le contenu de la zone de code assembleur passée en paramètre.
+     * @param zoneTexte la zone de saisie du code assembleur
+     * @return le code assembleur
+     * Je modifierai petut-être pour lever une exception si la zone assembleur est vide
+     * (dépendra des besoins des autres membres du groupe).
+     */
+    public String getAssembleur(JTextArea zoneTexte)
+    {
+        return zoneTexte.getText();
     }
 }
