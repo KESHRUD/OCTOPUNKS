169e015c07aad0751871dd943e80d2eafc1e7d73
Ahmed Amine Benhammada <123604915+KESHRUD@users.noreply.github.com>
2024-02-19 17:31:38 +0100

    Update File.java
    ajout de makefifo

--- src/fichiers/File.java
+++ src/fichiers/File.java
@@ -1,5 +1,3 @@
-package fichiers;
-
 import java.util.LinkedList;
 
 public class File extends Fichier {
@@ -35,5 +33,24 @@ public class File extends Fichier {
     public void vider() {
         file.clear();
         System.out.println("La file a été vidée.");
+    }
+
+    @Override
+    public void voidF() {
+        if (!file.isEmpty()) {
+            file.removeLast();
+            System.out.println("La dernière valeur de la file a été retirée.");
+        } else {
+            System.out.println("La file est vide !");
+        }
+    }
+
+    @Override
+    public boolean testEOF() {
+        return estVide();
+    }
+
+    public static File makeFIFO() {
+        return new File();
     }
 }
