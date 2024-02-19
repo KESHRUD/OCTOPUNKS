e4648f1516915ff6082b2c87ed434d6ff73a1788
Ahmed Amine Benhammada <123604915+KESHRUD@users.noreply.github.com>
2024-02-19 17:16:16 +0100

    Update Link.java
    Fonctionnalité bidirectionnel

--- src/main/Link.java
+++ src/main/Link.java
@@ -2,15 +2,19 @@ public class Link {
     private final int id;
     private final Salle salleAvant;
     private final Salle salleApres;
+    private final boolean bidirectionnel;
 
-    public Link(int id, Salle salle1, Salle salle2) {
+    public Link(int id, Salle salle1, Salle salle2, boolean bidirectionnel) {
         this.id = id;
         this.salleAvant = salle1;
         this.salleApres = salle2;
+        this.bidirectionnel = bidirectionnel;
 
-        // Définir la réciprocité du lien entre les salles
-        salleAvant.ajouterLienSortant(this);
-        salleApres.setLienEntrant(this);
+        // Définir la réciprocité du lien entre les salles si bidirectionnel est vrai
+        if (bidirectionnel) {
+            salleAvant.ajouterLienSortant(this);
+            salleApres.setLienEntrant(this);
+        }
     }
 
     public int getId() {
@@ -24,19 +28,8 @@ public class Link {
     public Salle getSalleApres() {
         return salleApres;
     }
-}
-/* 
     
-     * Méthode pour ajouter un lien à une salle.
-     * Cette méthode est appelée lors de la création du lien et sert à établir la réciprocité.
-     * @param lien Le lien à ajouter à la salle
-     *
-    private void ajouterLien(Link lien) {
-        if (this.equals(lien.getSalleAvant())) {
-            this.salleAvant.ajouterLienSortant(lien);
-        } else if (this.equals(lien.getSalleApres())) {
-            this.salleApres.ajouterLienSortant(lien);
-        }
+    public boolean isBidirectionnel() {
+        return bidirectionnel;
     }
 }
-*/
