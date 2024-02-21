a8da761759e06aaf77d18efe8c9ec2b86bbf7090
Ahmed Amine Benhammada <123604915+KESHRUD@users.noreply.github.com>
2024-02-21 20:45:46 +0100

    Create TypeFichier.java

---
+++ src/files/TypeFichier.java
@@ -0,0 +1,6 @@
+public abstract class TypeFichier<E> {
+    public abstract void add(E elt); //Ajoute un élément
+    public abstract void remove(); //Supprime un élément
+    public abstract void clear(); //Vide la structure
+    public abstract ListIterator<E> getIterator(int index); //Renvoie un itérateur sur la structure
+}
