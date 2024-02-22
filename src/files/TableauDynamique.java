c63994e6a3fdecedb93eb72c4c1ad3f25c2a086f
AbdoulayeHaidara1234 <152533121+AbdoulayeHaidara1234@users.noreply.github.com>
2024-02-22 11:38:11 +0100

    Update TableauDynamique.java

--- src/files/TableauDynamique.java
+++ src/files/TableauDynamique.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
-import java.util.Random;
 
 public class TableauDynamique<E> extends TypeFichier<E> {
     private ArrayList<E> liste;
@@ -22,65 +21,52 @@ public class TableauDynamique<E> extends TypeFichier<E> {
         return liste;
     }
 
+    
+    /** Pas sur */
     public void add(E elt) {
-        if (elt == null)
+        if(elt == null)
             throw new NullPointerException();
         liste.add(elt);
+    
     }
 
-    /** Renvoie la longueur du tableau dynamique */
+    /*Pas sur */
+    public void remove() {  
+        try {
+            liste.remove(index);
+        } catch (NoSuchElementException e) {
+            System.out.println("Il n'y a plus d'element dans la file");
+        }
+
+        if(index >= this.size())
+            index--;
+    }
+
+
     public int size() {
         return liste.size();
     }
 
     public void clear() {
         liste.clear();
+        liste = null;
     }
 
-    /** Renvoie un iterateur sur le tableau dynamique avec l'index du début du tableau */
+    /** Renvoie un iterateur sur la pile avec l'index du début de la pile */
     public ListIterator<E> getIterator(int index) {
         return liste.listIterator(index);
     }
 
-    /** Déplace le pointeur vers le prochain élément */
     public void nextIndex() {
         index++;
-        if (index >= size()) {
-            index = 0;
-        }
     }
 
-    /** Déplace le pointeur vers le précédent élément */
     public void previousIndex() {
         index--;
-        if (index < 0) {
-            index = size() - 1;
-        }
     }
 
-    /** Renvoie l'élément courant */
     public E getCurrentElement() {
         return liste.get(index);
-    }
-
-    /** Renvoie un tableau dynamique avec des valeurs aléatoires */
-    public static TableauDynamique<Integer> make(int length) {
-        TableauDynamique<Integer> tableau = new TableauDynamique<Integer>();
-        Random rand = new Random();
-
-        for (int i = 0; i < length; i++) {
-            tableau.add(rand.nextInt(100)); // Choisir la plage de nombres aléatoires selon vos besoins
-        }
-
-        return tableau;
-    }
-
-    /** Supprime l'élément courant */
-    public void voidF() {
-        liste.remove(index);
-        if (index >= size()) {
-            index--;
-        }
     }
 
 }
