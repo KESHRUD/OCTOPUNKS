b432234fd2fc33d095f908408cb0de4fab94d3fc
samy147 <152693322+samy147@users.noreply.github.com>
2024-03-01 21:09:49 +0100

    Update Champ.java ajout equals hashCode

--- src/model/Champ.java
+++ src/model/Champ.java
@@ -53,4 +53,16 @@ public class Champ {
     public void setContenu(String contenu) {
         this.contenu = contenu;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Champ)) return false;
+        Champ champ = (Champ) o;
+        return occupe == champ.occupe && Objects.equals(type, champ.type) && Objects.equals(contenu, champ.contenu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, occupe, contenu);
+    }
 }
