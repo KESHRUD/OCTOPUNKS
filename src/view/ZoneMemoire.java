4acf1bd5d9fb7ca8e5681d908b836df109edf27f
Stevekgs28 <84630495+Stevekgs28@users.noreply.github.com>
2024-03-16 21:42:46 +0100

    Update ZoneMemoire.java

--- src/view/ZoneMemoire.java
+++ src/view/ZoneMemoire.java
@@ -9,6 +9,7 @@ public class ZoneMemoire extends JPanel
     private Jeu jeu;
     private JLabel labelRobot1;
     private JLabel labelRobot2;
+    // private JLabel labelInfosFichier;
     private JLabel gameIsFinished;
     
     public ZoneMemoire(Octopunks octopunks, Jeu jeu)
@@ -20,12 +21,13 @@ public class ZoneMemoire extends JPanel
         this.setLocation((int)(0),(int)jeu.zoneAssembleur.getHeight()+this.getHeight());
         this.setBackground(Color.YELLOW);
     
-        this.labelRobot1 = new JLabel("XR1 : " + jeu.robot1.getPositionX() + " - YR1 : " + jeu.robot1.getPositionY() + " - Salle "+jeu.robot1.getCurrentSalle().getId());
+        this.labelRobot1 = new JLabel("XR1 : " + jeu.robot1.getPositionX() + " - YR1 : " + jeu.robot1.getPositionY() + " - Salle "+jeu.robot1.getCurrentSalle().getId()+" - xG : "+jeu.robot1.getPosition().getXGraphique() + " et yG : "+jeu.robot1.getPosition().getYGraphique());
+
         this.labelRobot1.setSize(this.getWidth()/2, this.getHeight()*1/4);
         this.labelRobot1.setLocation(0,(int)this.getHeight()*1/8);
         this.add(this.labelRobot1);
 
-        this.labelRobot2 = new JLabel("XR2 : " + jeu.robot2.getPositionX() + " - YR2 : " + jeu.robot2.getPositionY() + " - Salle "+jeu.robot2.getCurrentSalle().getId());
+        this.labelRobot2 = new JLabel("XR2 : " + jeu.robot2.getPositionX() + " - YR2 : " + jeu.robot2.getPositionY() + " - Salle "+jeu.robot2.getCurrentSalle().getId()+" - xG : "+jeu.robot2.getPosition().getXGraphique() + " et yG : "+jeu.robot2.getPosition().getYGraphique());
         this.labelRobot2.setSize(this.getWidth(), this.getHeight()*1/4);
         this.labelRobot2.setLocation(0,this.getHeight()*3/8);
         this.add(this.labelRobot2);
@@ -48,7 +50,7 @@ public class ZoneMemoire extends JPanel
      */
     public void update()
     {
-        this.labelRobot1.setText("XR1 : " + jeu.robot1.getPositionX() + " - YR1 : " + jeu.robot1.getPositionY() + " - Salle "+jeu.robot1.getCurrentSalle().getId());
-        this.labelRobot2.setText("XR2 : " + jeu.robot2.getPositionX() + " - YR2 : " + jeu.robot2.getPositionY() + " - Salle "+jeu.robot2.getCurrentSalle().getId());
+        this.labelRobot1.setText("XR1 : " + jeu.robot1.getPositionX() + " - YR1 : " + jeu.robot1.getPositionY() + " - Salle "+jeu.robot1.getCurrentSalle().getId()+" - xG : "+jeu.robot1.getRobotLabel().getX() + " et yG : "+jeu.robot1.getRobotLabel().getY());
+        this.labelRobot2.setText("XR2 : " + jeu.robot2.getPositionX() + " - YR2 : " + jeu.robot2.getPositionY() + " - Salle "+jeu.robot2.getCurrentSalle().getId()+" - xG : "+jeu.robot2.getRobotLabel().getX() + " et yG : "+jeu.robot2.getRobotLabel().getY());
     }
 }
